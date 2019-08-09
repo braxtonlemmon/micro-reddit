92d7da9f4e4c0f08a4941f208d28c176f10658fa
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-08-09 10:36:34 +0200

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	validates :name, presence: true, length: { in: 8..16 },
+									 uniqueness: true
+	validates :email, presence: { message: "cannot be blank" },
+										length: { maximum: 200 },
+										uniqueness: true
 end
